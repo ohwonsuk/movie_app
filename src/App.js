8db7849349f84384eeb43d2f1b701ac0e71ab672
오원석 <wonsuk@owonseog-ui-MacBookPro.local>
2023-04-23 18:45:38 +0900

    movie page styling 완료

--- src/App.js
+++ src/App.js
@@ -33,19 +33,21 @@ class App extends React.Component {
               <span className="loader__text">Loading...</span>
             </div>
           ) : (
-            movies.map((movie) => {
-              return (
-                <Movie
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                  genres={movie.genres}
-                />
-              );
-            })
+            <div className="movies">
+              {movies.map((movie) => {
+                return (
+                  <Movie
+                    key={movie.id}
+                    id={movie.id}
+                    year={movie.year}
+                    title={movie.title}
+                    summary={movie.summary}
+                    poster={movie.medium_cover_image}
+                    genres={movie.genres}
+                  />
+                );
+              })}
+            </div>
           )}
         </div>
       </section>
--- src/Movie.js
+++ src/Movie.js
@@ -8,15 +8,15 @@ function Movie({ year, title, summary, poster, genres }) {
       <img src={poster} alt={title} title={title} />
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
-        <h5 className="movie_year">{year}</h5>
-        <ul className="genres">
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
           {genres.map((genre, index) => (
             <li key={index} className="genres__genre">
               {genre}
             </li>
           ))}
         </ul>
-        <p className="movie_summary">{summary}</p>
+        <p className="movie_summary">{summary.slice(0, 180)}...</p>
       </div>
     </div>
   );
