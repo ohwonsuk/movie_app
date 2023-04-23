ac419de1705efbe4dc721c59b51fc9a16ad8c74f
오원석 <wonsuk@owonseog-ui-MacBookPro.local>
2023-04-23 11:53:18 +0900

    map 사용 리스트 자동 생성하기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React from 'react';
 
-function Food({fav}) {
+function Food({ name, picture }) {
 
-  return <h1>I love {fav}</h1>
+  return <div>
+    <h2>I like {name}</h2>
+    <img src={picture} alt={name} ></img>
+  </div>
 }
+
+const foodIlike = [
+  {
+    id: 1,
+    name: "kimchi",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQLo_gQFSt1FatcWZrlkjO1U8g-ZS75D2uDg&usqp=CAU"
+  },
+  {
+    id: 2,
+    name: "Samyeopsal",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn0h6IRD69vXW-dyvqtBlftTmJVWiTb4eC-A&usqp=CAU"
+  },
+  {
+    id: 3,
+    name: "Bibimbap",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBcfGT9xxIJJvCqfwQEv2O1LD0eAEww_ZofIjICOhRcSVxBAHdELU8qgj7E6Tex7zSMXw&usqp=CAU",
+  },
+  {
+    id: 4,
+    name: "Kimbap",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSp6yO2ikqocmAUI9gZsfCWP0oBmMRr2MfF8g&usqp=CAU"
+  }
+]
 
 function App() {
   return (
   <div>
-    <h1>Hello !!!</h1>
-    <Food fav="kimchi" />
-    <Food fav="ramen" />
-    <Food fav="samgiopsal" />
-    <Food fav="chukumi" />
+    {foodIlike.map(dish => <Food key={dish.id} name={dish.name} picture={dish.image} />
+    )}
   </div>
   );
 
